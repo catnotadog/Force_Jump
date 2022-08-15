7d3720f806014f50e15bf9b683c2306c76d62add
catnotadog <84086733+catnotadog@users.noreply.github.com>
2022-08-15 17:57:25 -0400

    fix compatibility with other mods

--- Force Jump/Main.cs
+++ Force Jump/Main.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using MelonLoader;
 using UnityEngine;
 
@@ -24,15 +24,16 @@ namespace Jump
 
             MelonLogger.Msg("Ui loaded");
 
-            var toinst = GameObject.Find("/UserInterface").transform.Find("Canvas_QuickMenu(Clone)/Container/Window/Wing_Right/Container/InnerContainer/WingMenu/ScrollRect/Viewport/VerticalLayoutGroup/Button_Emotes");
-            var inst = GameObject.Instantiate(toinst, toinst.parent).gameObject;
-            var txt = inst.transform.Find("Container/Text_QM_H3").GetComponent<TMPro.TextMeshProUGUI>();
-            txt.richText = true;
-            txt.text = $"<color=#000080ff>Jump</color>";
-            GameObject.DestroyImmediate(inst.transform.Find("Container/Icon").gameObject);
-            var btn = inst.GetComponent<UnityEngine.UI.Button>();
-            btn.onClick.RemoveAllListeners();
-            btn.onClick.AddListener(new System.Action(() => { VRC.SDKBase.Networking.LocalPlayer.SetJumpImpulse(4); VRC.Player.prop_Player_0.gameObject.GetComponent<CharacterController>().enabled = true; }));
+            var toinstjumpmod = GameObject.Find("/UserInterface").transform.Find("Canvas_QuickMenu(Clone)/Container/Window/Wing_Right/Container/InnerContainer/WingMenu/ScrollRect/Viewport/VerticalLayoutGroup/Button_Emotes");
+            var instjumpmod = GameObject.Instantiate(toinst, toinst.parent).gameObject;
+            instjumpmod.name = "Button jump"
+            var txtjumpmod = instjumpmod.transform.Find("Container/Text_QM_H3").GetComponent<TMPro.TextMeshProUGUI>();
+            txtjumpmod.richText = true;
+            txtjumpmod.text = $"<color=#000080ff>Jump</color>";
+            GameObject.DestroyImmediate(instjumpmod.transform.Find("Container/Icon").gameObject);
+            var btnjumpmod = instjumpmod.GetComponent<UnityEngine.UI.Button>();
+            btnjumpmod.onClick.RemoveAllListeners();
+            btnjumpmod.onClick.AddListener(new System.Action(() => { VRC.SDKBase.Networking.LocalPlayer.SetJumpImpulse(4); VRC.Player.prop_Player_0.gameObject.GetComponent<CharacterController>().enabled = true; }));
 
             
 
